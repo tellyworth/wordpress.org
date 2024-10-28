7535b256415ee8e9fefcb1cc8a0634e318ffd964
StevenDufresne <dufresnesteven@gmail.com>
2024-10-28 05:27:28 +0000

    Add in plugin-directory changes

---
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/shortcodes/class-release.php
@@ -0,0 +1,20 @@
+<?php
+namespace WordPressdotorg\Plugin_Directory\Shortcodes;
+
+use WordPressdotorg\Plugin_Directory\Plugin_Directory;
+use WordPressdotorg\Plugin_Directory\Template;
+
+/**
+ * The [wporg-plugins-releases] shortcode handler to display developer information.
+ *
+ * @package WordPressdotorg\Plugin_Directory\Shortcodes
+ */
+class Releases {
+
+	/**
+	 * @return string
+	 */
+	static function display() {
+		return '<ul>Not Implemented</ul>';
+	}
+}
