0a3ceef759615521fddd3bcad9ad02a1d8d0249b
tellyworth <alex@automattic.com>
2024-12-19 16:47:27 +1100

    Better setup and slightly more tests for support-reps
    Still WIP

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/tests/phpunit/tests/wporg-plugin-api-support-reps.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/tests/phpunit/tests/wporg-plugin-api-support-reps.php
@@ -1,22 +1,35 @@
 <?php
 
+use WordPressdotorg\Plugin_Directory\Tools;
+
 /**
  * @group new
  */
 class TestPluginApiSupportReps extends WP_Test_REST_Controller_Testcase {
 
 	public static $plugin_id;
+	public static $plugin_slug;
+	public static $support_rep;
+	public static $other_user;
 
 	public static function wpSetUpBeforeClass( WP_UnitTest_Factory $factory ) {
 
+		self::$plugin_slug = 'test-plugin';
 		self::$plugin_id  = $factory->post->create(
 			array(
 				'post_type' => 'plugin',
 				'post_modified' => current_time( 'mysql' ),
 				'post_modified_gmt' => current_time( 'mysql' ),
+				'post_name' => self::$plugin_slug,
 			)
 		);
+
+		self::$support_rep = $factory->user->create();
+		Tools::add_plugin_support_rep( self::$plugin_id, self::$support_rep );
+
+		self::$other_user = $factory->user->create();
 	}
+
 	public function test_register_routes() {
 		// Implement test_register_routes
 		$routes = rest_get_server()->get_routes();
@@ -27,11 +40,24 @@ class TestPluginApiSupportReps extends WP_Test_REST_Controller_Testcase {
 	}
 
 	public function test_context_param() {
-		// Implement test_context_param
+		// Collection.
+		$request  = new WP_REST_Request( 'OPTIONS', '/plugins/v1/plugin/' . self::$plugin_slug . '/support-reps' );
+		$response = rest_get_server()->dispatch( $request );
+		$data     = $response->get_data();
+
+		// TODO: what are we supposed to test here?
+
+		$this->markTestSkipped();
 	}
 
 	public function test_get_items() {
 		// Implement test_get_items
+		$request  = new WP_REST_Request( 'GET', '/plugins/v1/plugin/' . self::$plugin_slug . '/support-reps' );
+		$response = rest_get_server()->dispatch( $request );
+		$data     = $response->get_data();
+
+		$this->assertEquals( 200, $response->get_status() );
+		$this->assertEquals( self::$support_rep, $data[0]['id'] );
 	}
 
 	public function test_get_item() {
