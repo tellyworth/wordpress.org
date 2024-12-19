5c89b654c4a90616d0890495232193df0521b3f2
tellyworth <alex@automattic.com>
2024-12-19 14:23:37 +1100

    Use `wp-phpunit`

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/tests/bootstrap.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/tests/bootstrap.php
@@ -6,10 +6,20 @@ if ( 'cli' !== php_sapi_name() ) {
 	return;
 }
 
+// WP_TESTS_DOMAIN, WP_TESTS_EMAIL, WP_TESTS_TITLE, WP_PHP_BINARY
+
+define( 'WP_TESTS_DOMAIN', 'localhost:8881' ); // FIXME: can we fetch this dynamically?
+define( 'WP_TESTS_EMAIL', '' );
+define( 'WP_TESTS_TITLE', 'WordPress Plugin Directory Unit Tests' );
+define( 'WP_PHP_BINARY', 'php' );
+
 // load WordPress if we're running within wp-now.
 if ( false !== strpos( $_SERVER['_'], '@wp-now/wp-now' ) ) {
 	require_once '/var/www/html/wp-load.php';
 }
+
+require_once( dirname( __DIR__ ) . '/vendor/wp-phpunit/wp-phpunit/includes/bootstrap.php' );
+
 
 /**
  * Manually load the plugin being tested.
